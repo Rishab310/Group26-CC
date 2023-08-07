f2638eb3f3c441bb329c75dea62780a93ec5cdd7
Rishab Goyal <rishabgoyal432@gmail.com>
2023-08-07 18:25:34 +0530

    Remove unused routes

--- bonds-backend/app.js
+++ bonds-backend/app.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// const authRoutes = require('./routes/auth');
+// const classroomRoutes = require('./routes/classroom');
+
+// app.use('/auth', authRoutes);
+// app.use('/classes', classroomRoutes);
+
 app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.statusCode = 404;
@@ -18,19 +29,23 @@ app.use((err, req, res, next) => {
     console.log(err);
     const status = err.statusCode || 500;
     const message = err.message;
-    res.status(status).json({message: message});
+    res.status(status).json({ message: message });
 })
 
-mongoose.connect('mongodb+srv://rishikapatwa2002:DBnew2023@cluster0.pyvxnys.mongodb.net/?retryWrites=true&w=majority', 
-{ 
-    useUnifiedTopology: true, 
-    useNewUrlParser: true 
-})
-.then(result => {
-    app.listen(process.env.PORT || 5000);
-    console.log("Server started at port 5000");
-    console.log("http://localhost:5000");
-})
-.catch(err => {
-    console.log(err);
-})
+mongoose.connect('mongodb+srv://rishikapatwa2002:DBnew2023@cluster0.pyvxnys.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(result => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log("Server started at port 5000");
+        });
+        console.log("http://localhost:5000");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+// https://localhost:5000/auth/login -> post
+// https://localhost:5000/auth/signup -> put
