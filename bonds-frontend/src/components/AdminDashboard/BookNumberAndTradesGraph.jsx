4671c85de8d31bed5377929f41d0e3dcb02d3841
RamyaRajeshwari <83582648+RamyaRajeshwari@users.noreply.github.com>
2023-08-09 12:30:24 +0530

    frontend changes

--- bonds-frontend/src/components/AdminDashboard/BookNumberAndTradesGraph.jsx
+++ bonds-frontend/src/components/AdminDashboard/BookNumberAndTradesGraph.jsx
@@ -4,7 +4,7 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'rec
 const BookNumberAndTradesGraph = ({ data }) => {
   return (
     <div>
-      <h2>Number of Trades by Book</h2>
+      <h2>Number of Trades in Books</h2>
       <BarChart width={400} height={300} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="book" />
--- bonds-frontend/src/components/ManagerDashboard/Navbar.jsx
+++ bonds-frontend/src/components/ManagerDashboard/Navbar.jsx
@@ -4,14 +4,14 @@ export const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand p-2" href="#">
           Bonds App
         </a>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/adminDashboard">
+              <a className="nav-link" href="/managerDashboard">
                 View All Bonds
               </a>
             </li>
@@ -24,7 +24,7 @@ export const Navbar = () => {
         </div>
 
         <button
-          className="btn btn-outline-success my-2 my-sm-0 logOutButton"
+          className="btn btn-outline-primary my-2 my-sm-0 logOutButton p-2"
           type="submit"
         >
           Log Out
